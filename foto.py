68a8021e5f19621ed25b7d33ac236fb151ee4da7
new_world_12 <kirillsalamatov20@gmail.com>
2023-02-07 15:47:13 +0500

    как примерно будет выглядить структура данного файла

--- foto.py
+++ foto.py
@@ -3,4 +3,13 @@
 '''
 
 map_photo =  'https://drive.google.com/file/d/1nrZytzAXNc-migT8TN_Smf3fsjIcC_Iy/view?usp=sharing'
-first_photo = 'https://drive.google.com/file/d/1nNdhc9MRbpCr-z1gR2tjKY0DXwTG3_IW/view?usp=sharing'
+
+# номер вопроса, ссылка на фотографию, правильный ответ
+photo_1 = (1,'https://drive.google.com/file/d/1nNdhc9MRbpCr-z1gR2tjKY0DXwTG3_IW/view?usp=sharing', 29)
+photo_2 = (2,'https://drive.google.com/file/d/1AT0gfNxsEg3VRh_P0W0ENd6PELxAmIXD/view?usp=share_link', 1) 
+photo_3 = (3,'https://drive.google.com/file/d/1BK9zqejGgS2HiFgiXBOD5-RMGpNbyDbt/view?usp=share_link', 1)
+photo_4 = (4,'https://drive.google.com/file/d/1fl2B1YxN-PoP6dfXbXddM3Wryg-jaZIS/view?usp=share_link', 1)
+photo_5 = (5,'https://drive.google.com/file/d/10HCYiZGEsR_YZjH4LvWvZqlTuCxmb-fV/view?usp=share_link', 1)
+
+# все вопросы
+all_photos = [photo_1, photo_2, photo_3, photo_4, photo_5]
