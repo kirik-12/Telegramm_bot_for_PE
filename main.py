54ac47f47acb90274c6d2c35f3fcf73d86a06ba7
new_world_12 <kirillsalamatov20@gmail.com>
2023-02-11 00:45:18 +0500

    ВСОООООООООООООО!!!! Финальная версия

--- main.py
+++ main.py
@@ -28,6 +28,10 @@ class Question(StatesGroup):
     que_13 = State()
     que_14 = State()
     que_15 = State()
+    result_5 = State()
+    result_10 = State()
+    result_15 = State()
+
 
 # Здароваемся с нашей жертвой/ Даём кнопки 5,10,15 вопросов
 @bot.message_handler(commands=['start'])
@@ -38,9 +42,11 @@ def start_ex(message):
     global question_foto_ans 
     question_foto_ans = que_ft[0:5]
     markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    markup.add(types.KeyboardButton('/start_5'), types.KeyboardButton('10'), types.KeyboardButton('15'))
+    markup.add(types.KeyboardButton('/start_5'), types.KeyboardButton('/start_10'), types.KeyboardButton('/start_15'))
     bot.send_message(message.chat.id, 'Hi! Please, listen rules', reply_markup=markup)
     bot.send_photo(message.chat.id, ft.map_photo)
+
+
 
 #'5'. Начало теста с 5ью вопросами
 @bot.message_handler(commands=['start_5'])
@@ -83,14 +89,14 @@ def que_4_get(message):
 
 @bot.message_handler(state=Question.que_5)
 def que_5_get(message):
-    bot.send_message(message.chat.id, "Nice.Let`s look on you answer")
-    bot.set_state(message.from_user.id, Question.que_6, message.chat.id)
+    bot.send_message(message.chat.id, "Nice. Let`s look on you answer!")
+    bot.send_message(message.chat.id, 'Write anything...')
+    bot.set_state(message.from_user.id, Question.result_5, message.chat.id)
     with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
         data['que_5'] = message.text
 
- 
 # result
-@bot.message_handler(state=Question.que_6)
+@bot.message_handler(state=Question.result_5)
 def ready_for_answer(message):
     with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
         msg = ("Ready, take a look:\n<b>"
@@ -104,6 +110,275 @@ def ready_for_answer(message):
         results = data
         bot.send_message(message.chat.id, msg, parse_mode="html")
     bot.delete_state(message.from_user.id, message.chat.id)
+
+
+
+#'10'. Начало теста с 10ью вопросами 
+@bot.message_handler(commands=['start_10'])
+def start_ex(message):
+    bot.set_state(message.from_user.id, Question.que_1, message.chat.id)
+    bot.send_photo(message.chat.id, question_foto_ans[0][1])
+    bot.send_message(message.chat.id, text = '1. What is checkpoint?')
+
+@bot.message_handler(state=Question.que_1)
+def que_1_get(message):
+    bot.send_photo(message.chat.id, question_foto_ans[1][1])
+    bot.send_message(message.chat.id, text = '2. What is checkpoint?')
+    bot.set_state(message.from_user.id, Question.que_2, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['que_1'] = message.text
+ 
+@bot.message_handler(state=Question.que_2)
+def que_2_get(message):
+    bot.send_photo(message.chat.id, question_foto_ans[2][1])
+    bot.send_message(message.chat.id, "3. What is checkpoint?")
+    bot.set_state(message.from_user.id, Question.que_3, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['que_2'] = message.text
+
+@bot.message_handler(state=Question.que_3)
+def que_3_get(message):
+    bot.send_photo(message.chat.id, question_foto_ans[3][1])
+    bot.send_message(message.chat.id, "4. What is checkpoint?")
+    bot.set_state(message.from_user.id, Question.que_4, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['que_3'] = message.text
+
+@bot.message_handler(state=Question.que_4)
+def que_4_get(message):
+    bot.send_photo(message.chat.id, question_foto_ans[4][1])
+    bot.send_message(message.chat.id, "5. What is checkpoint?")
+    bot.set_state(message.from_user.id, Question.que_5, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['que_4'] = message.text
+
+@bot.message_handler(state=Question.que_5)
+def que_5_get(message):
+    bot.send_photo(message.chat.id, question_foto_ans[5][1])
+    bot.send_message(message.chat.id, "6. What is checkpoint?")
+    bot.set_state(message.from_user.id, Question.que_6, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['que_5'] = message.text
+
+@bot.message_handler(state=Question.que_6)
+def que_6_get(message):
+    bot.send_photo(message.chat.id, question_foto_ans[6][1])
+    bot.send_message(message.chat.id, "7. What is checkpoint?")
+    bot.set_state(message.from_user.id, Question.que_7, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['que_6'] = message.text
+
+@bot.message_handler(state=Question.que_7)
+def que_7_get(message):
+    bot.send_photo(message.chat.id, question_foto_ans[7][1])
+    bot.send_message(message.chat.id, "8. What is checkpoint?")
+    bot.set_state(message.from_user.id, Question.que_8, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['que_7'] = message.text
+
+@bot.message_handler(state=Question.que_8)
+def que_8_get(message):
+    bot.send_photo(message.chat.id, question_foto_ans[8][1])
+    bot.send_message(message.chat.id, "9. What is checkpoint?")
+    bot.set_state(message.from_user.id, Question.que_9, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['que_8'] = message.text
+
+@bot.message_handler(state=Question.que_9)
+def que_9_get(message):
+    bot.send_photo(message.chat.id, question_foto_ans[9][1])
+    bot.send_message(message.chat.id, "10. What is checkpoint?")
+    bot.set_state(message.from_user.id, Question.que_10, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['que_9'] = message.text
+
+@bot.message_handler(state=Question.que_10)
+def que_10_get(message):
+    bot.send_message(message.chat.id, "Nice. Let`s look on you answer!")
+    bot.send_message(message.chat.id, 'Write anything...')
+    bot.set_state(message.from_user.id, Question.result_10, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['que_10'] = message.text
+
+# result
+@bot.message_handler(state=Question.result_10)
+def ready_for_answer(message):
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        msg = ("Ready, take a look:\n<b>"
+               f"1: {data['que_1']}\n"
+               f"2: {data['que_2']}\n"
+               f"3: {data['que_3']}\n"
+               f"4: {data['que_4']}\n"
+               f"5: {data['que_5']}\n"
+               f"6: {data['que_6']}\n"
+               f"7: {data['que_7']}\n"
+               f"8: {data['que_8']}\n"
+               f"9: {data['que_9']}\n"
+               f"10: {data['que_10']}</b>\n"
+               'Write: "mark", looking for you results')
+        global results
+        results = data
+        bot.send_message(message.chat.id, msg, parse_mode="html")
+    bot.delete_state(message.from_user.id, message.chat.id)
+
+
+
+
+#'15'. Начало теста с 15ью вопросами 
+@bot.message_handler(commands=['start_15'])
+def start_ex(message):
+    bot.set_state(message.from_user.id, Question.que_1, message.chat.id)
+    bot.send_photo(message.chat.id, question_foto_ans[0][1])
+    bot.send_message(message.chat.id, text = '1. What is checkpoint?')
+
+@bot.message_handler(state=Question.que_1)
+def que_1_get(message):
+    bot.send_photo(message.chat.id, question_foto_ans[1][1])
+    bot.send_message(message.chat.id, text = '2. What is checkpoint?')
+    bot.set_state(message.from_user.id, Question.que_2, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['que_1'] = message.text
+ 
+@bot.message_handler(state=Question.que_2)
+def que_2_get(message):
+    bot.send_photo(message.chat.id, question_foto_ans[2][1])
+    bot.send_message(message.chat.id, "3. What is checkpoint?")
+    bot.set_state(message.from_user.id, Question.que_3, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['que_2'] = message.text
+
+@bot.message_handler(state=Question.que_3)
+def que_3_get(message):
+    bot.send_photo(message.chat.id, question_foto_ans[3][1])
+    bot.send_message(message.chat.id, "4. What is checkpoint?")
+    bot.set_state(message.from_user.id, Question.que_4, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['que_3'] = message.text
+
+@bot.message_handler(state=Question.que_4)
+def que_4_get(message):
+    bot.send_photo(message.chat.id, question_foto_ans[4][1])
+    bot.send_message(message.chat.id, "5. What is checkpoint?")
+    bot.set_state(message.from_user.id, Question.que_5, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['que_4'] = message.text
+
+@bot.message_handler(state=Question.que_5)
+def que_5_get(message):
+    bot.send_photo(message.chat.id, question_foto_ans[5][1])
+    bot.send_message(message.chat.id, "6. What is checkpoint?")
+    bot.set_state(message.from_user.id, Question.que_6, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['que_5'] = message.text
+
+@bot.message_handler(state=Question.que_6)
+def que_6_get(message):
+    bot.send_photo(message.chat.id, question_foto_ans[6][1])
+    bot.send_message(message.chat.id, "7. What is checkpoint?")
+    bot.set_state(message.from_user.id, Question.que_7, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['que_6'] = message.text
+
+@bot.message_handler(state=Question.que_7)
+def que_7_get(message):
+    bot.send_photo(message.chat.id, question_foto_ans[7][1])
+    bot.send_message(message.chat.id, "8. What is checkpoint?")
+    bot.set_state(message.from_user.id, Question.que_8, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['que_7'] = message.text
+
+@bot.message_handler(state=Question.que_8)
+def que_8_get(message):
+    bot.send_photo(message.chat.id, question_foto_ans[8][1])
+    bot.send_message(message.chat.id, "9. What is checkpoint?")
+    bot.set_state(message.from_user.id, Question.que_9, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['que_8'] = message.text
+
+@bot.message_handler(state=Question.que_9)
+def que_9_get(message):
+    bot.send_photo(message.chat.id, question_foto_ans[9][1])
+    bot.send_message(message.chat.id, "10. What is checkpoint?")
+    bot.set_state(message.from_user.id, Question.que_10, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['que_9'] = message.text
+
+@bot.message_handler(state=Question.que_10)
+def que_10_get(message):
+    bot.send_photo(message.chat.id, question_foto_ans[10][1])
+    bot.send_message(message.chat.id, "11. What is checkpoint?")
+    bot.set_state(message.from_user.id, Question.que_11, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['que_10'] = message.text
+
+@bot.message_handler(state=Question.que_11)
+def que_11_get(message):
+    bot.send_photo(message.chat.id, question_foto_ans[11][1])
+    bot.send_message(message.chat.id, "12. What is checkpoint?")
+    bot.set_state(message.from_user.id, Question.que_12, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['que_11'] = message.text
+
+@bot.message_handler(state=Question.que_12)
+def que_12_get(message):
+    bot.send_photo(message.chat.id, question_foto_ans[12][1])
+    bot.send_message(message.chat.id, "13. What is checkpoint?")
+    bot.set_state(message.from_user.id, Question.que_13, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['que_12'] = message.text
+
+@bot.message_handler(state=Question.que_13)
+def que_13_get(message):
+    bot.send_photo(message.chat.id, question_foto_ans[13][1])
+    bot.send_message(message.chat.id, "14. What is checkpoint?")
+    bot.set_state(message.from_user.id, Question.que_14, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['que_13'] = message.text
+
+@bot.message_handler(state=Question.que_14)
+def que_14_get(message):
+    bot.send_photo(message.chat.id, question_foto_ans[14][1])
+    bot.send_message(message.chat.id, "15. What is checkpoint?")
+    bot.set_state(message.from_user.id, Question.que_15, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['que_14'] = message.text
+
+
+@bot.message_handler(state=Question.que_15)
+def que_15_get(message):
+    bot.send_message(message.chat.id, "Nice. Let`s look on you answer!")
+    bot.send_message(message.chat.id, 'Write anything...')
+    bot.set_state(message.from_user.id, Question.result_15, message.chat.id)
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        data['que_15'] = message.text
+
+# result
+@bot.message_handler(state=Question.result_15)
+def ready_for_answer(message):
+    with bot.retrieve_data(message.from_user.id, message.chat.id) as data:
+        msg = ("Ready, take a look:\n<b>"
+               f"1: {data['que_1']}\n"
+               f"2: {data['que_2']}\n"
+               f"3: {data['que_3']}\n"
+               f"4: {data['que_4']}\n"
+               f"5: {data['que_5']}\n"
+               f"6: {data['que_6']}\n"
+               f"7: {data['que_7']}\n"
+               f"8: {data['que_8']}\n"
+               f"9: {data['que_9']}\n"
+               f"10: {data['que_10']}\n"
+               f"11: {data['que_11']}\n"
+               f"12: {data['que_12']}\n"
+               f"13: {data['que_13']}\n"
+               f"14: {data['que_14']}\n"
+               f"15: {data['que_15']}</b>\n"
+               'Write: "mark", looking for you results')
+        global results
+        results = data
+        bot.send_message(message.chat.id, msg, parse_mode="html")
+    bot.delete_state(message.from_user.id, message.chat.id)
+
+
 
 @bot.message_handler(content_types=['text'])
 def loli(message):
@@ -120,8 +395,8 @@ def loli(message):
 def age_incorrect(message):
     bot.send_message(message.chat.id, 'Please enter a number')
 
-# register filters
 
+# register filters
 bot.add_custom_filter(custom_filters.StateFilter(bot))
 bot.add_custom_filter(custom_filters.IsDigitFilter())
 
